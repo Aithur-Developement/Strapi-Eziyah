20ae5205172f92db42ccfa395b4accefe8e8cbaf
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2026-01-28 16:53:58 +0530

    Remove PUBLIC_URL from server configuration

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
 
-  url: env('PUBLIC_URL', 'http://strapi.eziyah.com.au'),
   app: {
     keys: env.array('APP_KEYS'),
   },
