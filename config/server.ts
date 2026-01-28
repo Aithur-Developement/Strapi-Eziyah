3c1215676095ca77c1c9467d074d6c78ea752df4
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2026-01-28 18:58:49 +0530

    Remove default URL from server configuration
    Removed the default URL configuration for the server.

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://databases-and-other-strapieziyah-e57d1s-4f7072-76-13-23-4.traefik.me'),
   app: {
     keys: env.array('APP_KEYS'),
   },
