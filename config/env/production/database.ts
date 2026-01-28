d947a15cbe34b46bacc4ee10c8137db742afdfd1
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2026-01-28 18:49:50 +0530

    Update database.ts

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-		host: env('DATABASE_HOST', '208.87.135.86'),
-			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'Validet'),
-			user: env('DATABASE_USERNAME', 'postgres'),
-			password: env('DATABASE_PASSWORD', '6cigvvxmsgss7tft'),
+		host: env('DATABASE_HOST'),
+			port: env.int('DATABASE_PORT'),
+			database: env('DATABASE_NAME'),
+			user: env('DATABASE_USERNAME'),
+			password: env('DATABASE_PASSWORD'),
 			ssl: env.bool('DATABASE_SSL', false)
 		}
 	}
