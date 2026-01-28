333defdb20514d11d4a64399b6ef2aefb35f3bbb
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2026-01-28 19:19:55 +0530

    Remove default URL from server configuration
    Remove default URL configuration from server settings.

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://strapi.ezyiah.com.au'),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
