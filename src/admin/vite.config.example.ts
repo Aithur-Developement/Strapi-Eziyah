df6703a42151356470cb8edbc150d39905ef4bce
Saifullah Khan <101410460+saifkp517@users.noreply.github.com>
2026-01-28 18:53:30 +0530

    Update vite.config.example.ts

--- src/admin/vite.config.example.ts
+++ src/admin/vite.config.example.ts
@@ -8,5 +8,13 @@ export default (config: UserConfig) => {
         '@': '/src',
       },
     },
+     server: {
+      allowedHosts: [
+        'databases-and-other-strapieziyah-e57d1s-4f7072-76-13-23-4.traefik.me',
+        '.traefik.me', // allows all traefik.me subdomains
+      ],
+      // Or use 'all' for development:
+      // allowedHosts: 'all',
+    },
   });
 };
